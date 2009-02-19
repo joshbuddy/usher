84809810d250adfbc41a523ae1fd428e0c4969e2
Joshua Hull <joshbuddy@gmail.com>
2009-02-19 08:55:44 -0500

    re-added specs

---
+++ spec/rails/generate_spec.rb
@@ -0,0 +1,61 @@
+require 'lib/compat'
+require 'lib/usher'
+
+route_set = ActionController::Routing::UsherRoutes
+
+describe "Usher URL generation" do
+  
+  before(:each) do
+    route_set.reset!
+  end
+  
+  it "should generate a simple URL" do
+    route_set.add_named_route(:sample, '/sample', :controller => 'sample', :action => 'action')
+    route_set.generate_url(:sample, {}).should == '/sample'
+  end
+  
+  it "should generate a simple URL with a single variable" do
+    route_set.add_named_route(:sample, '/sample/:action', :controller => 'sample')
+    route_set.generate_url(:sample, {:action => 'action'}).should == '/sample/action'
+  end
+  
+  it "should generate a simple URL with a single variable (thats not a string)" do
+    route_set.add_named_route(:sample, '/sample/:action/:id', :controller => 'sample')
+    route_set.generate_url(:sample, {:action => 'action', :id => 123}).should == '/sample/action/123'
+  end
+  
+  it "should generate a simple URL with a glob variable" do
+    route_set.add_named_route(:sample, '/sample/*action', :controller => 'sample')
+    route_set.generate_url(:sample, {:action => ['foo', 'baz']}).should == '/sample/foo/baz'
+  end
+  
+  it "should generate a mutliple vairable URL from a hash" do
+    route_set.add_named_route(:sample, '/sample/:first/:second', :controller => 'sample')
+    route_set.generate_url(:sample, {:first => 'zoo', :second => 'maz'}).should == '/sample/zoo/maz'
+  end
+
+  it "should generate a mutliple vairable URL from an array" do
+    route_set.add_named_route(:sample, '/sample/:first/:second', :controller => 'sample')
+    route_set.generate_url(:sample, ['maz', 'zoo']).should == '/sample/maz/zoo'
+  end
+
+  it "should generate append extra hash variables to the end" do
+    route_set.add_named_route(:sample, '/sample/:first/:second', :controller => 'sample')
+    route_set.generate_url(:sample, {:first => 'maz', :second => 'zoo', :third => 'zanz'}).should == '/sample/maz/zoo?third=zanz'
+  end
+
+  it "should generate append extra hash variables to the end using [] syntax if its an array" do
+    route_set.add_named_route(:sample, '/sample/:first/:second', :controller => 'sample')
+    route_set.generate_url(:sample, {:first => 'maz', :second => 'zoo', :third => ['zanz', 'susie']}).should == '/sample/maz/zoo?third%5B%5D=zanz&third%5B%5D=susie'
+  end
+
+  it "should default action to index when not present" do
+    route_set.add_route('/:controller/:action')
+    route_set.generate({:controller => 'sample'}, {}, :generate)
+  end
+
+  it "should leave off the action and id" do
+    route_set.add_route(':controller/:action/:id')
+    route_set.generate({:controller => 'controller'}, {}, :generate).should == '/controller'
+  end
+end
---
+++ spec/rails/recognize_spec.rb
@@ -0,0 +1,66 @@
+require 'lib/compat'
+require 'lib/usher'
+
+route_set = ActionController::Routing::UsherRoutes
+
+def build_request_mock(path, method, params)
+  request = mock "Request"
+  request.should_receive(:path).any_number_of_times.and_return(path)
+  request.should_receive(:method).any_number_of_times.and_return(method)
+  params = params.with_indifferent_access
+  request.should_receive(:path_parameters=).any_number_of_times.with(params)
+  request.should_receive(:path_parameters).any_number_of_times.and_return(params)
+  request
+end
+
+SampleController = Object.new
+
+describe "Usher route recognition" do
+
+  before(:each) do
+    route_set.reset!
+  end
+
+  it "should recognize a simple request" do
+    route_set.add_route('/sample', :controller => 'sample', :action => 'action')
+    route_set.recognize(build_request_mock('/sample', 'get', {:controller => 'sample', :action => 'action'})).should == SampleController
+  end
+
+  it "should interpolate action :index" do
+    route_set.add_route('/sample', :controller => 'sample')
+    route_set.recognize(build_request_mock('/sample', 'get', {:controller => 'sample', :action => 'index'})).should == SampleController
+  end
+
+  it "should correctly distinguish between multiple request methods" do
+    route_set.add_route('/sample', :controller => 'not_sample', :conditions => {:method => :get})
+    correct_route = route_set.add_route('/sample', :controller => 'sample', :conditions => {:method => :post})
+    route_set.add_route('/sample', :controller => 'not_sample', :conditions => {:method => :put})
+    route_set.recognize(build_request_mock('/sample', :post, {:controller => 'sample', :action => 'index'})).should == SampleController
+  end
+
+  it "should prefer the static route to the dynamic route" do
+    route_set.add_route('/sample/:action', :controller => 'not_sample')
+    route_set.add_route('/sample/test', :controller => 'sample', :action => 'action')
+    route_set.recognize(build_request_mock('/sample/test', 'get', {:controller => 'sample', :action => 'action'})).should == SampleController
+  end
+  
+  it "should raise based upon an invalid param" do
+    route_set.add_named_route(:sample, '/sample/:action', :controller => 'sample', :requirements => {:action => /\d+/})
+    proc { route_set.recognize(build_request_mock('/sample/asdqwe', :post, {})) }.should raise_error
+  end
+  
+  it "should add /:controller/:action if /:controller/:action/:id is added" do
+    route_set.add_route('/:controller/:action/:id')
+    route_set.route_count.should == 2
+  end
+  
+  it "should support root nodes" do
+    route_set.add_route('/sample', :controller => 'sample', :action => 'action')
+    route_set.add_route('/sample/:action', :controller => 'not_sample')
+    route_set.add_route('/sample/test', :controller => 'sample', :action => 'action')
+    route_set.add_route('/:controller/:action/:id')
+    route_set.add_route('/', :controller => 'sample')
+    route_set.recognize(build_request_mock('/', :get, {:controller => 'sample', :action => 'index'})).should == SampleController
+  end
+  
+end
