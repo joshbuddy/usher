2c60857326e1bd6b12f036fa5fa72461a6c92f2c
Joshua Hull <joshbuddy@gmail.com>
2009-04-02 10:37:07 -0400

    switched to autoload for these constant lookups

--- lib/usher/interface.rb
+++ lib/usher/interface.rb
@@ -2,17 +2,17 @@ $:.unshift File.dirname(__FILE__)
 
 class Usher
   module Interface
+    autoload :Rails2Interface, 'interface/rails2_interface'
+    autoload :MerbInterface, 'interface/merb_interface'
+    autoload :RackInterface, 'interface/rack_interface'
     
     def self.for(type)
       case type
       when :rails2
-        require 'interface/rails2_interface'
         Rails2Interface.new
       when :merb
-        require 'interface/merb_interface'
         MerbInterface.new
       when :rack
-        require 'interface/rack_interface'
         RackInterface.new
       end
     end
