8edd93be1812079da39d218fc68c0b51ed70976f
Daniel Neighman <dneighman@The-Sox.local>
2009-08-30 17:14:41 +1000

    Adds a hook for use by subclasses in the rack interface

--- lib/usher/interface/rack_interface.rb
+++ lib/usher/interface/rack_interface.rb
@@ -3,28 +3,28 @@ require 'rack'
 class Usher
   module Interface
     class RackInterface
-      
+
       attr_reader :router
-      
+
       class Builder < Rack::Builder
-        
+
         def initialize(&block)
           @usher = Usher::Interface::RackInterface.new
           super
         end
-        
+
         def map(path, options = nil, &block)
           @usher.add(path, options).to(&block)
           @ins << @usher unless @ins.last == @usher
         end
-        
+
       end
-      
+
       def initialize(&blk)
         @router = Usher.new(:request_methods => [:request_method, :host, :port, :scheme], :generator => Usher::Util::Generators::URL.new)
         instance_eval(&blk) if blk
       end
-      
+
       def dup
         new_one = super
         original = self
@@ -33,15 +33,15 @@ class Usher
         end
         new_one
       end
-      
+
       def add(path, options = nil)
         @router.add_route(path, options)
       end
-      
+
       def parent_route=(route)
         @router.parent_route = route
       end
-      
+
       def parent_route
         @router.parent_route
       end
@@ -51,7 +51,6 @@ class Usher
       end
 
       def call(env)
-        env['usher.params'] ||= {}
         response = @router.recognize(request = Rack::Request.new(env), request.path_info)
         if response.nil?
           body = "No route found"
@@ -60,19 +59,28 @@ class Usher
         else
           params = response.path.route.default_values || {}
           response.params.each{ |hk| params[hk.first] = hk.last}
-          
-          # consume the path_info to the script_name response.remaining_path
-          env["SCRIPT_NAME"] << response.matched_path   || ""
-          env["PATH_INFO"] = response.remaining_path    || ""
-                    
-          env['usher.params'].merge!(params)
-          
+
+          after_match(env, response)
+
           response.path.route.destination.call(env)
         end
       end
 
       def generate(route, params = nil, options = nil)
         @usher.generator.generate(route, params, options)
+      end
+
+      # Allows a hook to be placed for sub classes to make use of between matching
+      # and calling the application
+      def after_match(env, response)
+        env['usher.params'] ||= {}
+        params = response.path.route.default_values || {}
+        response.params.each{|hk| params[hk.first] = hk.last}
+        env['usher.params'].merge!(params)
+
+        # consume the path_info to the script_name response.remaining_path
+        env["SCRIPT_NAME"] << response.matched_path   || ""
+        env["PATH_INFO"] = response.remaining_path    || ""
       end
 
     end
