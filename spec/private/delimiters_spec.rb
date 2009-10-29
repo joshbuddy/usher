0ed232ae974ea8803e18338041e97985b0265d6f
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-10-29 19:33:28 +0600

    Correct search for first occurence of delimiters in path

--- spec/private/delimiters_spec.rb
+++ spec/private/delimiters_spec.rb
@@ -7,4 +7,42 @@ describe Delimiters do
       Delimiters.new(['/', '\)', '\\\\']).unescaped.should == ['/', ')', '\\']
     end
   end
+
+  describe "#first_in" do
+    describe "when there is a complex path with a lot of delimiters occurrences" do
+      before :each do
+        @delimiters = Delimiters.new ['@', '.', '/']
+        @paths = ['var', '.', 'var', '/', 'var', '@']
+      end
+
+      it "should find nearest delimiter correctly" do
+        @delimiters.first_in(@paths).should == '.'
+        @delimiters.first_in(@paths[2..-1]).should == '/'
+        @delimiters.first_in(@paths[4..-1]).should == '@'
+      end
+    end
+
+    describe "when there are delimiters with escaped charaters" do
+      before :each do
+        @delimiters = Delimiters.new ['\\(', '\\)']
+        @paths = ['var', '(', 'var', ')']
+      end
+
+      it "should find nearest delimiter in unescaped path" do
+        @delimiters.first_in(@paths).should == '('
+      end
+    end
+
+    describe "when there is no occurence of delimiters in path" do
+      before :each do
+        @delimiters = Delimiters.new ['-', '/']
+        @paths = ['e', '@', 'ma', '.', 'il']
+      end
+
+      it "should return nil" do
+        @delimiters.first_in(@paths).should be_nil
+      end
+    end
+    
+  end
 end
