f44bb7f9534691273d0705b430c3e2b17582e311
Joshua Hull <joshbuddy@gmail.com>
2009-11-27 16:29:05 -0500

    slightly more benchmarks

--- benchmarks/recognition_bm.rb
+++ benchmarks/recognition_bm.rb
@@ -3,14 +3,35 @@ require 'rbench'
 require 'lib/usher'
 
 u = Usher.new(:generator => Usher::Util::Generators::URL.new)
-u.add_route('/simple').name(:simple)
+u.add_route('/simple')
+u.add_route('/simple/again')
+u.add_route('/simple/again/and/again')
+u.add_route('/dynamic/:variable')
+u.add_route('/rails/:controller/:action/:id')
+u.add_route('/greedy/{!:greed,.*}')
 
-TIMES = 100_00
+TIMES = 50000
 
 RBench.run(TIMES) do
   
-  report "simple" do
+  report "2 levels, static" do
     u.recognize_path('/simple')
+  end
+  
+  report "4 levels, static" do
+    u.recognize_path('/simple/again')
+  end
+  
+  report "8 levels, static" do
+    u.recognize_path('/simple/again/and/again')
+  end
+  
+  report "4 levels, 1 dynamic" do
+    u.recognize_path('/dynamic/anything')
+  end
+  
+  report "4 levels, 1 greedy" do
+    u.recognize_path('/greedy/controller/action/id')
   end
   
 end
