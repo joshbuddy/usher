17d1015cc17e3cae100591ac71dc6ccd40c308ab
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-27 15:20:16 +0000

    Usher::Route#redirect in Rack interface improved & the method has robust tests now

--- lib/usher/interface/rack/route.rb
+++ lib/usher/interface/rack/route.rb
@@ -2,7 +2,10 @@ class Usher
   class Route
     # add("/index.html").redirect("/")
     def redirect(path, status = 302)
-      @destination = lambda do
+      unless (300..399).include?(status)
+        raise ArgumentError, "Status has to be an integer between 300 and 399"
+      end
+      @destination = lambda do |env|
         response = Rack::Response.new
         response.redirect(path, status)
         response.finish
