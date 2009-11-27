c31ffeca699330d9caa75ee0138d476c942d4beb
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-27 13:42:22 +0000

    Rack interface is working again

--- lib/usher/interface/rack/route.rb
+++ lib/usher/interface/rack/route.rb
@@ -1,17 +1,13 @@
 class Usher
-  module Interface
-    class Rack
-      module Route
-        # add("/index.html").redirect("/")
-        def redirect(path, status = 302)
-          @destination = lambda do
-            response = Rack::Response.new
-            response.redirect(path, status)
-            response.finish
-          end
-          return self
-        end
+  class Route
+    # add("/index.html").redirect("/")
+    def redirect(path, status = 302)
+      @destination = lambda do
+        response = Rack::Response.new
+        response.redirect(path, status)
+        response.finish
       end
+      return self
     end
   end
 end
