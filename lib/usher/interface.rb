cc1a0fbb0a645461afdc2d4f4db49c7e44abef51
Joshua Hull <joshbuddy@gmail.com>
2009-12-07 17:11:15 -0500

    added class_for

--- lib/usher/interface.rb
+++ lib/usher/interface.rb
@@ -17,16 +17,19 @@ class Usher
     register(:text,     File.join(File.dirname(__FILE__), 'interface', 'text'))
     register(:sinatra,  File.join(File.dirname(__FILE__), 'interface', 'sinatra'))
 
-    # Usher::Interface.for(:rack, &block)
-    def self.for(name, *args, &block)
+    def self.class_for(name)
       name = name.to_sym
       if InterfaceRegistry[name]
         require InterfaceRegistry[name]
-        const = Usher::Interface.const_get(File.basename(InterfaceRegistry[name]).to_s.split(/_/).map{|e| e.capitalize}.join)
-        const.new(*args, &block)
+        Usher::Interface.const_get(File.basename(InterfaceRegistry[name]).to_s.split(/_/).map{|e| e.capitalize}.join)
       else
         raise ArgumentError, "Interface #{name.inspect} doesn't exist. Choose one of: #{InterfaceRegistry.keys.inspect}"
       end
+    end
+
+    # Usher::Interface.for(:rack, &block)
+    def self.for(name, *args, &block)
+      class_for(name).new(*args, &block)
     end
 
   end
