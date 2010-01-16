f78dbf1090c995ebd95287b9568ef9264e3e2054
Joshua Hull <joshbuddy@gmail.com>
2010-01-16 14:35:00 -0500

    removed cruft

--- lib/usher/util.rb
+++ lib/usher/util.rb
@@ -2,7 +2,5 @@ class Usher
   module Util
     autoload :Generators, File.join('usher', 'util', 'generate')
     autoload :Parser,     File.join('usher', 'util', 'parser')
-    autoload :Graph,      File.join('usher', 'util', 'graph')
-    autoload :Mapper,     File.join('usher', 'util', 'mapper')
   end
 end
