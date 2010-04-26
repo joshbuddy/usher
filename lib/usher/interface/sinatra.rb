c5c4ac13c00637d0513d483f42da77374dc35e94
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-04-26 21:43:16 +0200

    Rewrite Sinatra Interface and added some tests.

--- lib/usher/interface/sinatra.rb
+++ lib/usher/interface/sinatra.rb
@@ -1,66 +1,122 @@
 class Usher
   module Interface
     class Sinatra
-      
-      module Extension
-        
-        def self.included(cls)
-          cls::Base.class_eval(<<-HERE_DOC, __FILE__, __LINE__)
-            def self.route(verb, path, options={}, &block)
-              @router ||= Usher.new(:request_methods => [:request_method, :host, :port, :scheme], :generator => Usher::Util::Generators::URL.new)
 
-              name = options.delete(:name)
-              options[:conditions] ||= {}
-              options[:conditions][:request_method] = verb
-              options[:conditions][:host] = options.delete(:host) if options.key?(:host)
-
-              # Because of self.options.host
-              host_name(options.delete(:host)) if options.key?(:host)
-
-              define_method "\#{verb} \#{path}", &block
-              unbound_method = instance_method("\#{verb} \#{path}")
-              block =
-                if block.arity != 0
-                  lambda { unbound_method.bind(self).call(*@block_params) }
-                else
-                  lambda { unbound_method.bind(self).call }
-                end
-
-              invoke_hook(:route_added, verb, path, block)
-
-              route = @router.add_route(path, options).to(block)
-              route.name(name) if name
-              route
-            end          
-
-            def self.router
-              @router
-            end
-
-            def route!(base = self.class)
-              if self.class.router and match = self.class.router.recognize(@request, @request.path_info)
-                @block_params = match.params.map{|p| p.last}
-                @params = @params ? @params.merge(match.params_as_hash) : match.params_as_hash
-                route_eval(&match.destination)
-              elsif base.superclass.respond_to?(:routes)
-                route! base.superclass
-              else
-                route_missing
-              end
-            end
-            
-            def generate(name, *params)
-              self.class.router.generator.generate(name, *params)
-            end
-
-          HERE_DOC
-        end
-        
-      end
-      
       def initialize
         ::Sinatra.send(:include, Extension)
       end
-    end
-  end
-end
+
+      module Extension
+
+        def self.registered(app)
+          app.send(:include, Extension)
+        end
+
+        def self.included(base)
+          base.extend ClassMethods
+        end
+
+        def generate(name, *params)
+          self.class.generate(name, *params)
+        end
+
+        private
+          def route!(base=self.class, pass_block=nil)
+            if base.router and match = base.router.recognize(@request, @request.path_info)
+              @block_params = match.params.map { |p| p.last }
+              (@params ||= {}).merge!(match.params_as_hash)
+              pass_block = catch(:pass) do
+                route_eval(&match.destination)
+              end
+            end
+
+            # Run routes defined in superclass.
+            if base.superclass.respond_to?(:router)
+              route! base.superclass, pass_block
+              return
+            end
+
+            route_eval(&pass_block) if pass_block
+
+            route_missing
+          end
+
+        module ClassMethods
+
+          def new(*args, &bk)
+            configure! unless @_configured
+            super(*args, &bk)
+          end
+
+          def route(verb, path, options={}, &block)
+            path.gsub!(/\/\?$/, "(/)")
+            name = options.delete(:name)
+            options[:conditions] ||= {}
+            options[:conditions][:request_method] = verb
+            options[:conditions][:host] = options.delete(:host) if options.key?(:host)
+
+            define_method "#{verb} #{path}", &block
+            unbound_method = instance_method("#{verb} #{path}")
+            block =
+              if block.arity != 0
+                lambda { unbound_method.bind(self).call(*@block_params) }
+              else
+                lambda { unbound_method.bind(self).call }
+              end
+
+            invoke_hook(:route_added, verb, path, block)
+
+            route = router.add_route(path, options).to(block)
+            route.name(name) if name
+            route
+          end
+
+          def router
+            @router ||= Usher.new(:request_methods => [:request_method, :host, :port, :scheme],
+                                  :ignore_trailing_delimiters => true,
+                                  :generator => Usher::Util::Generators::URL.new)
+            block_given? ? yield(@router) : @router
+          end
+
+          def generate(name, *params)
+            router.generator.generate(name, *params)
+          end
+
+          def reset!
+            router.reset!
+            super
+          end
+
+          def configure!
+            configure :development do
+              error 404 do
+                content_type 'text/html'
+
+                (<<-HTML).gsub(/^ {17}/, '')
+                <!DOCTYPE html>
+                <html>
+                <head>
+                  <style type="text/css">
+                  body { text-align:center;font-family:helvetica,arial;font-size:22px;
+                    color:#888;margin:20px}
+                  #c {margin:0 auto;width:500px;text-align:left}
+                  </style>
+                </head>
+                <body>
+                  <h2>Sinatra doesn't know this ditty.</h2>
+                  <div id="c">
+                    Try this:
+                    <pre>#{request.request_method.downcase} '#{request.path_info}' do\n  "Hello World"\nend</pre>
+                  </div>
+                </body>
+                </html>
+                HTML
+              end
+            end
+            @_configured = true
+          end
+        end # ClassMethods
+      end # Extension
+    end # Sinatra
+  end # Interface
+end # Usher
---
+++ spec/private/sinatra/recognize_spec.rb
@@ -0,0 +1,71 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
+require "usher"
+require "sinatra"
+
+describe "Usher (for Sinatra) route recognition" do
+  before(:each) do
+    @app = Sinatra.new { register Usher::Interface::Sinatra::Extension }
+    @app.extend(CallWithMockRequestMixin)
+    @app.reset!
+  end
+
+  describe "basic functionality" do
+    it "should map not found" do
+      response = @app.call_with_mock_request('/bar')
+      response.status.should == 404
+    end
+
+    it "should map index" do
+      @app.get("/") { "index" }
+      response = @app.call_with_mock_request('/')
+      response.status.should == 200
+      response.body.should == "index"
+    end
+
+    it "should ignore trailing delimiters" do
+      @app.get("/foo") { "foo" }
+      response = @app.call_with_mock_request('/foo')
+      response.status.should == 200
+      response.body.should == "foo"
+      response = @app.call_with_mock_request('/foo/')
+      response.status.should == 200
+      response.body.should == "foo"
+    end
+
+    it "should use sinatra optionals trailing delimiters" do
+      @app.get("/foo/?") { "foo" }
+      response = @app.call_with_mock_request('/foo')
+      response.status.should == 200
+      response.body.should == "foo"
+      response = @app.call_with_mock_request('/foo/')
+      response.status.should == 200
+      response.body.should == "foo"
+    end
+  end
+
+  describe "mapping functionality" do
+
+    it "should map a basic route" do
+      @app.get('/hi', :name => :hi) { generate(:hi) } 
+      response = @app.call_with_mock_request('/hi')
+      response.status.should == 200
+      response.body.should == "/hi"
+    end
+
+    it "should map a basic route with params" do
+      @app.get('/hi/:id', :name => :hi) { generate(:hi, :id => 18) } 
+      response = @app.call_with_mock_request('/hi/1')
+      response.status.should == 200
+      response.body.should == "/hi/18"
+    end
+  end
+
+  describe "not found" do
+
+    it "should correctly generate a not found page without images" do
+      response = @app.call_with_mock_request('/bar')
+      response.status.should == 404
+      response.body.should_not match(/__sinatra__/)
+    end
+  end
+end
